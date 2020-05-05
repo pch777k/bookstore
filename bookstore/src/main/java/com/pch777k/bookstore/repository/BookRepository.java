bbd3ad424f9815885506c81fa0263ffe53ac5a70
Pawel Chmiel <pch777@gazeta.pl>
2020-05-05 19:00:43 +0200

    create angular project

--- bookstore/src/main/java/com/pch777k/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/pch777k/bookstore/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.pch777k.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.pch777k.bookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
