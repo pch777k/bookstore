86a1e33d471807ea4d946b28e1be828e94b74a13
Pawel Chmiel <pch777@gazeta.pl>
2020-05-06 10:19:17 +0200

    add missing file

--- bookstore-angular/src/app/services/book.service.ts
+++ bookstore-angular/src/app/services/book.service.ts
@@ -9,7 +9,7 @@ import { Book } from '../common/book';
 })
 export class BookService {
 
-  private baseUrl = 'http://localhost:8080/api/v1/books';
+  private baseUrl = 'http://localhost:8080/api/v1/books;
 
   constructor(private httpClient: HttpClient) { }
 
