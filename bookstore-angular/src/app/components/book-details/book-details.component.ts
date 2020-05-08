a4be4fbdabbffedc04fea4f078d7f0925102d4ea
Pawel Chmiel <pch777@gazeta.pl>
2020-05-08 11:50:50 +0200

    create bookDetails page

---
+++ bookstore-angular/src/app/components/book-details/book-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent implements OnInit {
+
+book: Book = new Book();
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(
+      () => {
+        this.getBookInfo();
+      }
+    )
+  }
+
+  getBookInfo(){
+    const id: number = +this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.bookService.getBook(id).subscribe(
+      data => {
+        this.book = data;
+      }
+    )
+  }
+}
